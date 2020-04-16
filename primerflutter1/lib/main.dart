ebbac2f115e29f76a50d488deef058d1aeeaa5f9
iteap <ca.vizcaino.g@itchilpancingo.edu.mx>
2020-04-16 18:03:48 -0500

    se modifico el textbox

--- primerflutter1/lib/main.dart
+++ primerflutter1/lib/main.dart
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Recuerda que no debes presionar el botón:',
             ),
             Text(
               '$_counter',
